32b4fb7b6e3a341006f0b8cd93f0c2c396ddb346
chiragverma04 <vermachirag4678@gmail.com>
2022-01-26 20:44:28 +0530

    changed sendcomic

--- send_comic2.php
+++ send_comic2.php
@@ -1,11 +1,11 @@
 <?php
 include "database.php";
-  include "send1.php"; 
-
+include "send1.php"; 
+include "username.php";
 $rand_num = rand(1,1000);
 $url_xkcd = "https://xkcd.com/".$rand_num."/info.0.json";
 
-//call api
+
 $json_data = file_get_contents($url_xkcd);
 $json_decoded = json_decode($json_data, true);
                                                                  
@@ -23,16 +23,16 @@ $success=file_put_contents($file_path, file_get_contents($image_url));
 
    if($success)
 {
-echo "Image File is saved now! ";
+echo "<h1>Mail Sent to Your Email Address</h1>";
+echo "<h1>Go and Enjoy Your XKCD Comics</h1>";
 }
 
-require 'vendor/autoload.php';
 
-$sendgrid_apikey = ("$Api");
-$sendgrid = new SendGrid($sendgrid_apikey);
 $url = 'https://api.sendgrid.com/';
+$user = "$user1";
+$pass = "$Api";
 
-$pass = $sendgrid_apikey;
+
 $filename = "RTCAMP.png";
 $filePath = dirname(__FILE__);
 
@@ -43,12 +43,14 @@ $query = "SELECT * FROM `reg_members` WHERE subscribed= '1' ";
 
 $res = $conn->query($query); 
 
-//if(mysqli_num_rows($res) > 0){
+
   
   while($rows=$res->fetch_assoc()){
     $to = $rows['emailid'];
 
     $params = array(
+      'api_user' => $user,
+      'api_key'  => $pass,
       'to'        => "$to",
       'toname'    => "XKCD Comic Lover",
       'from'      => "vermachirag4678@gmail.com",
@@ -56,32 +58,27 @@ $res = $conn->query($query);
     'subject'   => " XKCD Comics ",
     'text'      => "I'm text!",
     'html'      =>  "<img src=" .$image_url." alt=Random image />"." <a href='http://xkcd-image.herokuapp.com/unsubscribe.php?email=$to'>"." <h4>Unsubscribe XKCD!</h4></a>",
+
     'files['.$filename.']' => new CurlFile($file_name_with_full_path, $filetype, $filename)
   );
   
-print_r($params);
+
 $request =  $url.'api/mail.send.json';
 
-// Generate curl request
-$session = curl_init($request);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
 
-//curl_setopt($session, CURLOPT_HTTPHEADER, array("Content-Type: image/png")); 
-// Tell curl to use HTTP POST
+$session = curl_init($request);
 curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
+curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $pass));
+
 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
 curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
-// obtain response
+
 $response = curl_exec($session);
 curl_close($session);
-
-// print everything out
 print_r($response);
+
 }
 ?>
